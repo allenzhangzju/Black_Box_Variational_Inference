9bd29e3274aec76da18390b6098191d7dc57e1f7
lzj-isee <3160104370@zju.edu.cn>
2019-11-28 15:50:08 +0800

    2019/11/28

---
+++ experiment_blr/bbvi_null.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+from  torch.utils.data.dataloader import DataLoader
+from torchvision import transforms
+from data_load import DatasetFromCSV
+from functions import*
+'''
+bbvi without Rao_Blackwellization and Control Variates
+'''
+num_epochs=10
+batchSize=128
+S=5
+dim=28*28+1
+eta=0.2
+#读取数据
+transform=transforms.ToTensor()
+train_data=DatasetFromCSV('./train_images_csv.csv','./train_labels_csv.csv',transforms=transform)
+test_data=DatasetFromCSV('./test_images_csv.csv','./test_labels_csv.csv',transforms=transform)
+train_loader=DataLoader(train_data,batch_size=batchSize,shuffle=False)
+
+#定义变量，前28*28是权重w，最后一个是偏差项bias
+mu_s=torch.zeros(dim,requires_grad=True)
+log_sigma2_s=torch.zeros(dim,requires_grad=True)
+
+#需要储存结果
+elbo_list=[]
+variance_list=[]
+accuracy_list=[]
+
+#AdaGrad
+G=torch.zeros((dim*2,dim*2))
+
+#开始迭代
+for epoch in range(num_epochs):
+    for i ,data in enumerate(train_loader):
+        images,labels=data_preprocess(data)
+        #过程变量
+        gradient=torch.zeros((dim*2,S))
+        elbo=torch.zeros(S)
+        mu1=np.zeros(S)
+        #采样
+        for s in range(S):
+            z_sample=sampleZ(mu_s,log_sigma2_s,dim)
+            log_p=log_P(images,labels,z_sample,dim)
+            log_q=log_Q(mu_s,log_sigma2_s,z_sample)
+            log_q.backward()#这里用自动求导
+            elbo[s]=log_p-log_q#这里记录elbo
+            gradient[0:dim,s]=mu_s.grad*elbo[s]#这里记录梯度
+            gradient[dim:,s]=log_sigma2_s.grad*elbo[s]
+            mu1[s]=gradient[0,s].item()#这里记录μ1的梯度
+            mu_s.grad.data.zero_()#清除梯度，为准备下一次迭代
+            log_sigma2_s.grad.data.zero_()
+        elbo_list.append(np.mean(elbo.detach().numpy()))#求elbo的均值加入list
+        grad=gradient.mean(1)#求梯度均值
+        G+=torch.matmul(grad.view(dim*2,-1),grad.view(-1,dim*2))#AdaGrad
+        rho=eta/torch.sqrt(torch.diag(G))#AdaGrad
+        mu_s.data+=torch.mul(rho[0:dim],grad[0:dim])#step
+        log_sigma2_s.data+=torch.mul(rho[dim:],grad[dim:])#step
+        accuracy_list.append(accuracyCalc(mu_s,log_sigma2_s,test_data,dim))#在测试集上计算accuracy
+        variance_list.append(mu1_varianceCalc(mu1))#计算μ1的方差
+        print('{:.8f}, {:.8f}, {:.8f}'.format(\
+            elbo_list[len(elbo_list)-1],
+            accuracy_list[len(accuracy_list)-1],
+            variance_list[len(variance_list)-1]))
+        
+
--- experiment_blr/functions.py
+++ experiment_blr/functions.py
@@ -10,15 +10,13 @@ def log_Q(mu_s,log_sigma2_s,z_sample):
     normal=torch.distributions.normal.Normal(mu_s,std)
     return torch.sum(normal.log_prob(z_sample),0)
 
-def log_P(images,labels,z_sample,dim,num):
+def log_P(images,labels,z_sample,dim):
     normal=torch.distributions.normal.Normal(torch.zeros(dim),torch.ones(dim))
     log_prior=torch.sum(normal.log_prob(z_sample),0)
     a=torch.matmul(images,z_sample)
     log_likelihood=torch.sum(torch.log(torch.sigmoid(torch.mul(a,labels))),0)
     return log_likelihood+log_prior
 
-def grad_log_Q(mu_s,log_sigma2_s,z_sample):
-    pass
 
 def accuracyCalc(mu_s,log_sigma2_s,test_data,dim):
     images=torch.tensor(test_data.images.values/255).float()
@@ -30,3 +28,11 @@ def accuracyCalc(mu_s,log_sigma2_s,test_data,dim):
     
 def mu1_varianceCalc(mu1):
     return np.var(np.array(mu1))
+
+def data_preprocess(data):
+    images=data[0].view(-1,28*28)
+    lens=len(images)
+    labels=data[1].view(lens)
+    images=torch.div(images.float(),255)
+    images=torch.cat([images,torch.ones((lens,1))],1)#补bias
+    return images,labels
