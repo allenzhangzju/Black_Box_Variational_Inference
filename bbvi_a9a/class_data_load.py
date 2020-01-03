74a11fceb9da48625b52defdbbca4eac65a2e6b2
lzj-isee <51073165+lzj-isee@users.noreply.github.com>
2020-01-03 23:58:50 +0800

    2020/1/3

---
+++ bbvi_a9a/class_data_load.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from torch import optim,nn
+import torch.nn.functional as func
+from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt 
+
+class DatasetFromCSV(Dataset):
+    def __init__(self,image_path,label_path,transforms=None):
+        self.images=pd.read_csv(image_path,header=None)
+        self.labels=pd.read_csv(label_path,header=None)
+        self.transforms=transforms
+    
+    def __getitem__(self,index):
+        single_label=np.asarray(self.labels.iloc[index])
+        single_image=np.asarray(self.images.iloc[index]).reshape(1,123)
+        if self.transforms is not None:
+            image_tensor=self.transforms(single_image)
+            #label_tensor=self.transforms(single_label)
+            return (image_tensor,single_label)
+        return (single_image,single_label)
+    
+    def __len__(self):
+        return len(self.images.index)
+'''
+transform=transforms.ToTensor()
+train_data=DatasetFromCSV('./dataset/train_images_csv.csv','./dataset/train_labels_csv.csv',transforms=transform)
+train_loader=DataLoader(train_data,batch_size=10,shuffle=True)
+for i,data in enumerate(train_loader):
+    images=data[0].view(-1,28,28)[0]
+    print(data[1])
+    plt.imshow(images)
+    plt.show()
+
+    a=1
+b=1
+'''
