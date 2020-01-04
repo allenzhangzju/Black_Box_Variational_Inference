fb02baf862425a2d173cc8825bc9342f84bbf624
lzj-isee <51073165+lzj-isee@users.noreply.github.com>
2020-01-04 23:47:31 +0800

    2020/1/4

--- bbvi_mushrooms/abbvi_basic.py
+++ bbvi_mushrooms/abbvi_basic.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import torch
 from  torch.utils.data.dataloader import DataLoader
@@ -5,31 +6,33 @@ from torchvision import transforms
 from class_data_load import DatasetFromCSV
 from functions import*
 import os
+import sys
+path=sys.argv[1]
 '''
 abbvi without any extension
 '''
 num_epochs=50
-batchSize=1500
+batchSize=680
 num_S=5#训练的采样数量
-dim=123+1#这里+1是偏置
+dim=112+1#这里+1是偏置
 #eta=0.05#eta、k、w、c这四个参数是和论文对应的
-#k=0.2  w=1e10  c=1e8
-#k=0.2  w=2e9  c=0.7e8
-k=0.2
-w=8e10
-c=4e8
+#k=0.3  w=0.5e10 c=0.3e8
+#k=0.3  w=0.1e10 c=0.12e8  不太稳
+k=0.3
+w=0.5e10
+c=0.25e8
 M=10
-num_St=1000#测试的采样数量
+num_St=5000#测试的采样数量
 #读取数据
 transform=transforms.ToTensor()
 train_data=DatasetFromCSV('./dataset/train_images_csv.csv','./dataset/train_labels_csv.csv',transforms=transform)
-test_data=DatasetFromCSV('./dataset/test_images_csv.csv','./dataset/test_labels_csv.csv',transforms=transform)
+#test_data=DatasetFromCSV('./dataset/test_images_csv.csv','./dataset/test_labels_csv.csv',transforms=transform)
 train_loader=DataLoader(train_data,batch_size=batchSize,shuffle=True)
 
 #定义分布参数
 para=torch.zeros(dim*2,requires_grad=True)
 #para[dim:]=torch.ones(dim)*(-1)
-scale=32561/batchSize
+scale=8124/batchSize
 
 
 #需要储存结果
@@ -48,8 +51,8 @@ for epoch in range(num_epochs):
         images,labels=data_preprocess(data)
         revise=batchSize/len(images)
         #ELBO evaluate & record para
-        if (epoch*len(train_loader)+i)%10==0:
-            para_list.append(para.clone().detach().numpy())
+        #para_list.append(para.clone().detach().numpy())
+        if (epoch*len(train_loader)+i)%5==0:
             elbo_list.append(elbo_evaluate(images,labels,para,dim,scale,revise,num_St).item())
         #算法起始位置
         if(epoch==0 and i==0):
@@ -79,7 +82,7 @@ for epoch in range(num_epochs):
         grad_d=(1-b)*(grad_d+Delta)+b*nabla_F
         print(b,torch.median(update.abs()),torch.max(update.abs()))
         #print information
-        if (epoch*len(train_loader)+i)%10==0:
+        if  (epoch*len(train_loader)+i)%5==0:
             print('Epoch[{}/{}], step[{}/{}]'.format(\
                 epoch+1,
                 num_epochs,
@@ -87,7 +90,7 @@ for epoch in range(num_epochs):
             print('ELBO: {:.3f}\n'.format(\
                 elbo_list[len(elbo_list)-1]))
 
-
+'''
 if not os.path.exists('./result_elbo'):
     os.makedirs('./result_elbo')
 result=np.array(elbo_list)
@@ -98,3 +101,6 @@ if not os.path.exists('./result_para'):
     os.makedirs('./result_para')
 result=np.array(para_list)
 np.save('./result_para/abbvi_basic.npy',result)
+'''
+result=np.array(elbo_list)
+np.save(path,result)
---
+++ bbvi_mushrooms/control.py
@@ -0,0 +1,10 @@
+import os
+
+for i in range(0,5):
+    print('bbvi_basic')
+    #os.system('python bbvi_basic.py ./elbos/bbvi_basic/{:}.npy'.format(i))
+    print('bbvi_cv')
+    #os.system('python bbvi_cv.py ./elbos/bbvi_cv/{:}.npy'.format(i))
+    print('abbvi_basic')
+    os.system('python abbvi_basic.py ./elbos/abbvi_basic/{:}.npy'.format(i))
+    
